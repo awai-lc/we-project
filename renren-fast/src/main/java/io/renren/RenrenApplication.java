2f86d1d140007533db26a9f9fa8bdd3fb6cbdd8f
liucheng <36628353>
2020-07-05 20:02:11 +0800

    add by liucheng 2020/07/05-2

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -12,7 +12,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class RenrenApplication {
